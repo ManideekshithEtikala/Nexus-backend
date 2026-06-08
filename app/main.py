5613c6201332449f2e2e5f6d5371ab4592e56a92
manideekshith <manideekshithgaming@gmail.com>
2026-06-08 19:14:37 +0530

    fix: remove trailing slash from CORS origin

--- app/main.py
+++ app/main.py
@@ -31,7 +31,11 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://nexus-frontned.vercel.app/"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://nexus-frontned.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
